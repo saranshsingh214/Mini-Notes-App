a0c91d02b531ea02a601cf6573fb0d0df59d4dd9
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2024-06-29 16:40:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    fs.readdir(`./files`,function(err, files){
+        res.render("index",{files:files});
+    })
+});
+
+//dynamic routing
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`,"utf-8", function(err,filedata){
+        res.render("show",{filename: req.params.filename,filedata: filedata});
+    });
+});
+
+app.get('/edit/:filename', (req, res) => {
+    res.render("edit",{filename: req.params.filename});
+});
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`,req.body.details,function(err){
+        res.redirect('/')
+    });
+});
+
+app.post('/edit', (req, res) => {
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+        res.redirect('/')
+    })
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
