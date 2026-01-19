921d616732b729b457839cd60984545329208fbd
Saransh Singh <111219765+saranshsingh214@users.noreply.github.com>
2026-01-19 20:40:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.disable('x-powered-by');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
     fs.readdir(`./files`,function(err, files){
